c994563ef66172a4dbf1c01a1674655de3012092
Shane Howe <shanehowe100@gmail.com>
2025-12-07 16:02:27 +0000

    dont print stack trace

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,7 @@
 import java.io.IOException;
 
 public class Main {
-  public static void main(String[] args){
-    try {
-      ToyHttpServer.run();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+  public static void main(String[] args) throws IOException {
+    ToyHttpServer.run();
   }
 }
